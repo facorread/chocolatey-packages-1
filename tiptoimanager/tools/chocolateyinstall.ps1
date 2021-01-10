9ed3734c476f9022d103fdfc0dff8f0f4183fff3
RalfEs73 <ralf@essbaumer.de>
2021-01-10 19:32:47 +0100

    New Choco Packet

---
+++ tiptoimanager/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.ravensburger.de/db/installer_flat/tiptoi_Manager_Installer.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  
+  softwareName  = 'tiptoimanager*'
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
