e15a7aa79d8c750b2e2392c9762cf146366f8f52
RalfEs73 <ralf@essbaumer.de>
2021-01-10 22:04:36 +0100

    Add AnyStream

---
+++ anystream/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://www.redfox.bz/download/SetupAnyStream32.exe'
+$url64      = 'https://www.redfox.bz/download/SetupAnyStream64.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'anystream*'
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
