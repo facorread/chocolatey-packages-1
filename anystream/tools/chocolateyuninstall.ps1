bcecce6de4af082dd4ae8da8eb1ad2dc85210092
RalfEs73 <ralf@essbaumer.de>
2021-01-10 22:43:34 +0100

    Update

--- anystream/tools/chocolateyuninstall.ps1
+++ anystream/tools/chocolateyuninstall.ps1
@@ -1,30 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
  
 $packageName = 'anystream'
-$registryUninstallerKeyName_32 = 'AnyDVD'
-$registryUninstallerKeyName_64 = 'AnyDVD64'
 $shouldUninstall = $true
- 
-$local_key     = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName_64"
-$local_key6432   = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName_32"
-$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName_64"
-$machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName_32"
- 
-$file = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
-    | ?{ Test-Path $_ } `
-    | Get-ItemProperty `
-    | Select-Object -ExpandProperty UninstallString
- 
-if ($file -eq $null -or $file -eq '') {
-    Write-Host "$packageName has already been uninstalled by other means."
-    $shouldUninstall = $false
-}
+$file = "C:\Program Files\RedFox\AnyStream\AnyStream-uninst.exe /S"
 
 $installerType = 'EXE'
 $silentArgs = "/S"
 $validExitCodes = @(0)
-
-$file = $file -replace "/D",($silentArgs+" /D")
  
 if ($shouldUninstall) {
  Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -validExitCodes $validExitCodes -File $file
