505e3fd9e3fade79c54bd94d34b1e592b13faf78
RalfEs73 <ralf@essbaumer.de>
2021-01-11 14:20:57 +0100

    Update

--- cloudberrybackup/tools/chocolateyinstall.ps1
+++ cloudberrybackup/tools/chocolateyinstall.ps1
@@ -1,14 +1,16 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/cb_setups/_current/cbbackup/setup.exe?X-Amz-Expires=604800&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJGX5FT3LRU2F4MBQ/20210111/us-east-1/s3/aws4_request&X-Amz-Date=20210111T124921Z&X-Amz-SignedHeaders=host&response-content-disposition=attachment;filename=CloudBerryBackup_v7.0.0.648DESKTOP_c920681.exe&X-Amz-Signature=f2fcbca97a6d250f36634f91377b878235e031119da56a20119c6a7bf8dc2c69'
-$url64      = ''
+# $url        = 'https://s3.amazonaws.com/cb_setups/_current/cbbackup/setup.exe?X-Amz-Expires=604800&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJGX5FT3LRU2F4MBQ/20210111/us-east-1/s3/aws4_request&X-Amz-Date=20210111T124921Z&X-Amz-SignedHeaders=host&response-content-disposition=attachment;filename=CloudBerryBackup_v7.0.0.648DESKTOP_c920681.exe&X-Amz-Signature=f2fcbca97a6d250f36634f91377b878235e031119da56a20119c6a7bf8dc2c69'
+# $url64      = ''
+
+$file_Zip       = Join-Path $toolsDir 'CloudBerryBackup_v7.0.0.648.exe'
+
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
-  url64bit      = $url64
+  file          = $file_Zip
   softwareName  = 'cloudberrybackup*'
   checksum      = ''
   checksumType  = 'sha256'
