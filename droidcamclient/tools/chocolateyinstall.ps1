5902584091d4404b7c64a06b9b325cc107f2dee4
RalfEs73 <ralf@essbaumer.de>
2021-01-12 09:18:59 +0100

    New Packet DroidCam Client

---
+++ droidcamclient/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://files.dev47apps.net/win/DroidCam.Setup.6.4.1.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'droidcamclient*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
