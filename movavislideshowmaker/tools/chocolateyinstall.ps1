544a51f91f6ac91e25c6277a2afbe7aaebdc3fba
RalfEs73 <ralf@essbaumer.de>
2021-01-13 17:37:45 +0100

    Add Movavi Slideshow Video Maker

---
+++ movavislideshowmaker/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://files.movavi.com/x32/MovaviSlideshowMakerSetup.exe'
+$url64      = 'https://files.movavi.com/x64/MovaviSlideshowMakerSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'movavislideshowmaker*'
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
