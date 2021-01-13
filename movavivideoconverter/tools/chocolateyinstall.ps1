bb0a5da4318b7680a4556276284d1b9ad687a5e8
RalfEs73 <ralf@essbaumer.de>
2021-01-13 13:14:27 +0100

    Add Movavi Video Converter

---
+++ movavivideoconverter/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://files.movavi.com/x32/MovaviVideoConverterSetupE.exe'
+$url64      = 'https://files.movavi.com/x64/MovaviVideoConverterSetupE.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'movavivideoconverter*'
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
