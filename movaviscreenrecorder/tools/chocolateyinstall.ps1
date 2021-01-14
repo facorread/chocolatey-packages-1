b8bb943dbe01a1be37efce4ba9b1930d3b865933
RalfEs73 <ralf@essbaumer.de>
2021-01-14 17:35:12 +0100

    Add Movavi Screen Recorder

---
+++ movaviscreenrecorder/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://files.movavi.com/x32/MovaviScreenRecorderSetupE.exe'
+$url64      = 'https://files.movavi.com/x64/MovaviScreenRecorderSetupE.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'movaviscreenrecorder*'
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
