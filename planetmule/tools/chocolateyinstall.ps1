e0e068cabca0fe353734ceebca15af2d53ad7c97
RalfEs73 <ralf@essbaumer.de>
2021-01-15 22:56:47 +0100

    Small Updates

--- planetmule/tools/chocolateyinstall.ps1
+++ planetmule/tools/chocolateyinstall.ps1
@@ -1,25 +1,23 @@
-﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$url        = ''
-$url64      = ''
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'planetmule' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\planetmule.zip"
+$checksum    = '2E1FDB66EA9808D0F56A89A70657F3CC4DD7464DACB5C3EC9B79C49BC3901815'
+$Shortcut    = 'Mule.lnk'
+$ProgramEXE  = 'mule.exe'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'ZIP'
-  url           = $url
-  url64bit      = $url64
-
-  softwareName  = 'planetmule*'
-  checksum      = ''
-  checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
-  
-
-}
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
 
 Install-ChocolateyZipPackage @packageArgs
+
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+
+Remove-Item $url | out-null
