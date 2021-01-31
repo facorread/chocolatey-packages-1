27cfb847f8af04a448f03dca8530043c82262643
RalfEs73 <ralf@essbaumer.de>
2021-01-31 21:45:05 +0100

    Update to Version 1.0.9.0

--- anystream/tools/chocolateyinstall.ps1
+++ anystream/tools/chocolateyinstall.ps1
@@ -11,9 +11,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'anystream*'
-  checksum      = ''
+  checksum      = 'CECDB177E9C1F15121457D78AE1910292530D62353D068AD4143ED7DA8E06048'
   checksumType  = 'sha256'
-  checksum64    = ''
+  checksum64    = 'BB45B6117D2E5F0E365E40649F7DC112D438C2C7B2D653404B1F2B8C1E180846'
   checksumType64= 'sha256'
   silentArgs   = '/S'
 }
