aebbf701c4fcd0854655c7600c5c7809d108838a
RalfEs73 <ralf@essbaumer.de>
2021-02-04 15:15:18 +0100

    Add Thumbico

---
+++ thumbico/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.aurelitec.com/thumbico/windows/thumbico-install.exe'
+$url64      = 'https://download.aurelitec.com/thumbico/windows/thumbico-install.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'thumbico*'
+
+  checksum      = '4B7BE6842F95A3252633B30B2AFD292A25D5794FF7C7C1B03B382474B5A3D167'
+  checksumType  = 'sha256'
+  checksum64    = '4B7BE6842F95A3252633B30B2AFD292A25D5794FF7C7C1B03B382474B5A3D167'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
