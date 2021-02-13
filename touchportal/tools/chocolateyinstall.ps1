607befad64360a0076f3e44296c26b4839aa1d38
RalfEs73 <ralf@essbaumer.de>
2021-02-13 12:57:24 +0100

    Add Touch Portal

---
+++ touchportal/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = 'https://www.touch-portal.com/downloads/TouchPortal_Setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'touchportal*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = 'https://www.touch-portal.com/downloads/TouchPortal_Setup.exe'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
