c2461ebcf039b69879c14599a88c9f42d7fb1ff2
RalfEs73 <ralf@essbaumer.de>
2021-02-13 22:51:55 +0100

    Small Updates

--- cloudberrybackup/tools/chocolateyinstall.ps1
+++ cloudberrybackup/tools/chocolateyinstall.ps1
@@ -1,14 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file_Zip       = Join-Path $toolsDir 'CloudBerryBackup_v7.0.0.648.exe'
+$url        = 'https://download.msp360.com/CloudBerryBackup_v7.0.0.648DESKTOP.exe'
+$url64      = ''
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  file          = $file_Zip
+  url           = $url
+  url64bit      = $url64
+
   softwareName  = 'cloudberrybackup*'
-  checksum      = ''
+
+  checksum      = '0FA1151C8EE12ADB71CC1D03925F3D9392AB06EE03C44E7DBA39960E0A5FDF49'
   checksumType  = 'sha256'
   checksum64    = ''
   checksumType64= 'sha256'
