9a8d67e4483fae2aa08e8f50c16e2d4befe9e042
RalfEs73 <ralf@essbaumer.de>
2021-02-18 11:02:25 +0100

    Add Exodus Wallet

---
+++ exoduswallet/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://downloads.exodus.com/releases/exodus-windows-x64-21.2.12.exe'
+$url64      = 'https://downloads.exodus.com/releases/exodus-windows-x64-21.2.12.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'exoduswallet*'
+  checksum      = 'C4C44ECF0BC420AE2FCA550BF66ACD0E54E03C918A31227C4F4401D8C87AFAD0'
+  checksumType  = 'sha256'
+  checksum64    = 'C4C44ECF0BC420AE2FCA550BF66ACD0E54E03C918A31227C4F4401D8C87AFAD0'
+  checksumType64= 'sha256'
+  silentArgs   = '/s /v"/qn"'
+}
+
+Install-ChocolateyPackage @packageArgs
