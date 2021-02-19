849a649bfd33143cc296d605c8e89a1ef6bc07f8
RalfEs73 <ralf@essbaumer.de>
2021-02-19 11:00:51 +0100

    Small Updates

--- powerautomatedesktop/tools/chocolateyinstall.ps1
+++ powerautomatedesktop/tools/chocolateyinstall.ps1
@@ -1,21 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://download.microsoft.com/download/b/d/8/bd8409df-7b80-4ef7-89c5-5a7a941a5093/Setup.Microsoft.PowerAutomateDesktop.exe'
-$url64      = 'https://download.microsoft.com/download/b/d/8/bd8409df-7b80-4ef7-89c5-5a7a941a5093/Setup.Microsoft.PowerAutomateDesktop.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'powerautomatedesktop*'
 
   checksum      = '77546DA22D28148A356CC3453718D388B52C59851B9721E92001ECF13AD4643E'
   checksumType  = 'sha256'
-  checksum64    = '77546DA22D28148A356CC3453718D388B52C59851B9721E92001ECF13AD4643E'
-  checksumType64= 'sha256'
 
   silentArgs   = '/S -accepteula'
 }
