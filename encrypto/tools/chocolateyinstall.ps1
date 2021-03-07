a12156c67f883757d0cfa79d76b91f9f3dafe3c5
RalfEs73 <ralf@essbaumer.de>
2021-03-07 20:47:42 +0100

    Add Encrypto

---
+++ encrypto/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.devmate.com/com.macpaw.win.Encrypto/EncryptoforWin.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'encrypto*'
+
+  checksum      = '841C55824F88B7FD6D1474ADB8E4F7AA854677DFAF1CE4542768DD99EB73AB59'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
