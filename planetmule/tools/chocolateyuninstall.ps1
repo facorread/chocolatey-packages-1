19d35c01e19e5ec0e1e8f3a14c86f1bdef5d86e8
RalfEs73 <ralf@essbaumer.de>
2021-03-08 21:16:05 +0100

    Small Updates

--- planetmule/tools/chocolateyuninstall.ps1
+++ planetmule/tools/chocolateyuninstall.ps1
@@ -4,6 +4,6 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $Shortcut    = 'Mule.lnk'
 
 
-Remove-Item -Path "$env:Public\Desktop\$Shortcut"
-Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -Force -Confirm
-Remove-Item "$toolsDir\Mule" -Recurse -Force -Confirm
+Remove-Item -Path "$env:Public\Desktop\$Shortcut" -Y
+Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -Y
+Remove-Item "$toolsDir\Mule" -Recurse -Force -Y
