6916453888d7b86c3ce7e15c8223141d11e588ec
RalfEs73 <ralf@essbaumer.de>
2021-03-10 21:20:19 +0100

    Small Updates

--- planetmule/tools/chocolateyinstall.ps1
+++ planetmule/tools/chocolateyinstall.ps1
@@ -3,7 +3,6 @@ $packageName = 'planetmule'
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checksum    = '2E1FDB66EA9808D0F56A89A70657F3CC4DD7464DACB5C3EC9B79C49BC3901815'
 $Shortcut    = 'Mule.lnk'
 $ProgramEXE  = 'mule.exe'
 
