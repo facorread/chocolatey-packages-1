f6ed1f4a09d9bd4875910a242e5628530884ba3a
RalfEs73 <ralf@essbaumer.de>
2021-03-24 09:41:50 +0100

    New Version 7.2.1

--- movavislideshowmaker/tools/chocolateyinstall.ps1
+++ movavislideshowmaker/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'movavislideshowmaker*'
-  checksum      = '4E68456FC7D36CCF035A684D427955CA9A7977B360321E8E6820ABC7028393F4'
+  checksum      = '9257574C82062730A905F95B23D785A3C41E2FB45DBBDEB8BCD043D4A9B48915'
   checksumType  = 'sha256'
   silentArgs   = '/S'
 }
