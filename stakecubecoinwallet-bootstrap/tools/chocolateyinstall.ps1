2e3061ade3a3a82f2d1bc5b739626df06a21c441
RalfEs73 <ralf@essbaumer.de>
2021-04-01 21:43:00 +0200

    Small Updates

--- stakecubecoinwallet-bootstrap/tools/chocolateyinstall.ps1
+++ stakecubecoinwallet-bootstrap/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $packageName		= 'stakecubecoinwallet-bootstrap'
 
 $toolsPath			= Split-Path $MyInvocation.MyCommand.Definition
 $toolsDir			= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$toolsDestination = Join-Path $env:appdata "StakeCubeCoin"
-$checksum64		    = '83826ACAB663AAF6F26FF552B6DAA0DCDE4A3576FFC48D741DC43061C5FB8C0F'
+$toolsDestination	= Join-Path $env:appdata "StakeCubeCoin"
+$checksum_file	    = '83826ACAB663AAF6F26FF552B6DAA0DCDE4A3576FFC48D741DC43061C5FB8C0F'
 
 
 $packageArgs = @{
@@ -13,7 +13,9 @@ $packageArgs = @{
     Destination		= $toolsDir
 }
 
-Get-ChocolateyWebFile -PackageName 'stakecubecoinwallet-bootstrap' -FileFullPath "$toolsPath\bootstrap.zip" -Url 'https://github.com/stakecube/StakeCubeCoin/releases/download/v3.1.0/bootstrap.zip'
+# Get-ChocolateyWebFile -PackageName 'stakecubecoinwallet-bootstrap' -FileFullPath "$toolsPath\bootstrap.zip" -Url 'https://github.com/stakecube/StakeCubeCoin/releases/download/v3.1.0/bootstrap.zip'
+Get-ChocolateyWebFile -Checksum $checksum_file -ChecksumType 'SHA256' -PackageName 'stakecubecoinwallet-bootstrap' -FileFullPath "$toolsPath\bootstrap.zip" -Url 'https://github.com/stakecube/StakeCubeCoin/releases/download/v3.1.0/bootstrap.zip'
+
 Get-ChocolateyUnzip @packageArgs
 
 New-Item -Path "$toolsDestination" -ItemType directory -Force | Out-Null
