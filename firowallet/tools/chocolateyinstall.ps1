4ff92087c0aed21acc084a60eaf1266f02e8b99b
RalfEs73 <ralf@essbaumer.de>
2021-04-15 22:40:50 +0200

    New Version 0.14.6.0

--- firowallet/tools/chocolateyinstall.ps1
+++ firowallet/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/firoorg/firo/releases/download/v0.14.5.3/firo-0.14.5.3-win64-setup.exe'
+$url64      = 'https://github.com/firoorg/firo/releases/download/v0.14.6.0/firo-0.14.6.0-win64-setup.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'firowallet*'
-  checksum64    = 'E62FE7FC6B48CE091F590850DEFC58290C92D7BA54F1AA54DBDB4706B6A36C1F'
+  checksum64    = 'E87098F375519820EA9DA0244E8E87A5C87C855745DC102E8B6DC428D8295D3C'
   checksumType64= 'sha256'
   silentArgs   = '/S'
 }
