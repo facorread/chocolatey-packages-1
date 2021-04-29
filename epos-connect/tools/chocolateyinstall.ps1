5935050a28f95c0912be4c86e02df8d1c64f07ed
RalfEs73 <ralf@essbaumer.de>
2021-04-29 20:07:37 +0200

    First Version

---
+++ epos-connect/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE_MSI_OR_MSU'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'test*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  # MSI
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+  # OTHERS
+  # Uncomment matching EXE type (sorted by most to least common)
+  #silentArgs   = '/S'           # NSIS
+  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  #silentArgs   = '/s'           # InstallShield
+  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
+  #silentArgs   = '/s'           # Wise InstallMaster
+  #silentArgs   = '-s'           # Squirrel
+  #silentArgs   = '-q'           # Install4j
+  #silentArgs   = '-s'           # Ghost
+  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
+                                 #       https://chocolatey.org/packages/autohotkey.portable
+  #validExitCodes= @(0) #please insert other valid exit codes here
+}
+
+Install-ChocolateyPackage @packageArgs
