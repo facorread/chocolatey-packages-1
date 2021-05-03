ec98f4ac69f2938002c6153a8e83d86ef5789fbb
RalfEs73 <ralf@essbaumer.de>
2021-05-03 13:20:50 +0200

    First Version

---
+++ chia-network/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/Chia-Network/chia-blockchain/releases/download/1.1.3/ChiaSetup-1.1.3.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'chia-network*'
+
+  checksum      = 'B510A6687E211D77A41BE500988155B5E042CBAAFAFC34B0BA45217A96302C2B'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
